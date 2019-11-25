74340bd7833958cb9a1d4e7c7d4124e97c55d563
Derek Crosson <derek@designcafe.co.za>
2019-11-25 03:51:11 +0200

    Draw game board.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![deny(missing_docs)]
-
 extern crate piston;
 extern crate glutin_window;
 extern crate graphics;
@@ -19,17 +17,16 @@ mod gameboard;
 mod gameboard_controller;
 mod gameboard_view;
 
+
+
 fn main() {
-  let settings = WindowSettings::new("Sudoku", [512; 2])
-      .exit_on_esc(true);
-  let window: GlutinWindow = settings.build()
-      .expect("Could not create window");
-  
   let opengl = OpenGL::V3_2;
   let settings = WindowSettings::new("Sudoku", [512; 2])
-      .graphic_api(opengl)
+      .graphics_api(opengl)
       .exit_on_esc(true);
-  
+  let mut window: GlutinWindow = settings.build()
+      .expect("Could not create window");
+
   let mut events = Events::new(EventSettings::new().lazy(true));
   let mut gl = GlGraphics::new(opengl);
   
@@ -39,7 +36,9 @@ fn main() {
   let gameboard_view = GameboardView::new(gameboard_view_settings);
 
   while let Some(e) = events.next(&mut window) {
-      gameboard_controller.event(&e);
+      gameboard_controller.event(gameboard_view.settings.position,
+                                   gameboard_view.settings.size,
+                                   &e);
       if let Some(args) = e.render_args() {
           gl.draw(args.viewport(), |c, g| {
               use graphics::{clear};
