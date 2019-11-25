b7d5b96525a0ac5d16bb76936d9fff23d149550f
Derek Crosson <derek@designcafe.co.za>
2019-11-25 04:17:44 +0200

    Draw numbers.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use piston::window::WindowSettings;
 use piston::event_loop::{Events, EventLoop, EventSettings};
 use piston::input::RenderEvent;
 use glutin_window::GlutinWindow;
-use opengl_graphics::{OpenGL, GlGraphics};
+use opengl_graphics::{OpenGL, Filter, GlGraphics, GlyphCache, TextureSettings};
 
 pub use crate::gameboard::Gameboard;
 pub use crate::gameboard_controller::GameboardController;
@@ -16,8 +16,6 @@ pub use crate::gameboard_view::{GameboardView, GameboardViewSettings};
 mod gameboard;
 mod gameboard_controller;
 mod gameboard_view;
-
-
 
 fn main() {
   let opengl = OpenGL::V3_2;
@@ -35,6 +33,10 @@ fn main() {
   let gameboard_view_settings = GameboardViewSettings::new();
   let gameboard_view = GameboardView::new(gameboard_view_settings);
 
+  let texture_settings = TextureSettings::new().filter(Filter::Nearest);
+  let ref mut glyphs = GlyphCache::new("assets/FiraSans-Regular.ttf", (), texture_settings)
+      .expect("Could not load font");
+
   while let Some(e) = events.next(&mut window) {
       gameboard_controller.event(gameboard_view.settings.position,
                                    gameboard_view.settings.size,
@@ -44,7 +46,7 @@ fn main() {
               use graphics::{clear};
 
               clear([1.0; 4], g);
-              gameboard_view.draw(&gameboard_controller, &c, g);
+              gameboard_view.draw(&gameboard_controller, glyphs, &c, g);
           });
       }
   }
